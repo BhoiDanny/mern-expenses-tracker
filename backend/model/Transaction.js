380407004dcf2c9e123808f64e2fefb7fe477370
Cypherios <brownssmith899@gmail.com>
2024-06-13 21:21:01 +0000

    chore(models): add model schema structure

---
+++ backend/model/Transaction.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ["income", "expense"],
+        },
+        category: {
+            type: String,
+            required: true,
+            default: "Uncategorized",
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+        description: {
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.model("Transaction", transactionSchema);
