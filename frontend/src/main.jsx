939cb1c2562a5832d8f633f33e66f6f4ce4a8ebd
Cypherios <brownssmith899@gmail.com>
2024-06-15 13:55:36 +0000

    chore: add tanstack query and configurations

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './redux/store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store/store.js";
+
+//! instance of react query
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
